cb74062c0fe16bc48054eeea6b73ee45e2d3127f
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-10-27 10:33:33 +0000

    test on all data

--- long_test_nq.py
+++ long_test_nq.py
@@ -11,6 +11,8 @@ MODEL = '4bit/Llama-2-7b-chat-hf'
 def evaluate_llm(
         llm: HybridMethod,
         beta: float,
+        time_0: float,
+        max_time: float,
         data: List[Any],
         dola_layers_good: Union[Literal['high', 'low'], None] = None,
         dola_layers_bad: Union[Literal['high', 'low'], None] = None,
@@ -21,7 +23,7 @@ def evaluate_llm(
     """
     score: int = 0
     print("Length of data", len(data))
-    for idx, qa in enumerate(data[0:100]):
+    for idx, qa in enumerate(data):
         context: str = qa["context"]
         question: str = qa["question"]
         answers: List[str] = qa["answer"]
@@ -36,10 +38,15 @@ def evaluate_llm(
         )
 
         if cad_answer == None: return -1  # Error with CAD generation
-        
+
+        if time.time() - time_0 >= max_time:
+            print("Time up!")
+            return score
+
         if idx % 100 == 0:
             print(f"{idx}. CAD answer: {repr(normalize_answer(cad_answer))}")
             print(f"{idx}. Correct answers:", " ".join([repr(normalize_answer(answers[i])) for i in range(len(answers))]))
+            print("Time:", time.time() - time_0)
         if evaluate_nq_ans(cad_answer, answers):
             score += 1
     print(f"RESULT: CAD with coefficient={beta}, dola-good set to {dola_layers_good}, dola-bad set to {dola_layers_bad}, model {llm.model_name}, we achieved a score of {score}/860")
@@ -79,6 +86,8 @@ if __name__ == "__main__":
         score: int = evaluate_llm(
             llm=llm,
             beta=beta,
+            time_0=time_0,
+            max_time=9*60,
             data=data,
             dola_layers_good=dola_layers_good,
             dola_layers_bad=dola_layers_bad,
@@ -86,10 +95,10 @@ if __name__ == "__main__":
         results[str(beta)] = score
         ex_time = time.time() - time_1
         tot_time = time.time() - time_0
-        if tot_time * 60 >= 9: # 9 mins
-            break
-
         print(f"Evaluation time for beta={beta}: {ex_time:.4f}s")
+        if tot_time / (60 * 60) >= 7.0: # 9 mins
+            print("7 hrs up")
+            break
 
     print("Final results:", results)
     with open(f'nq_cad_dola_{str(dola_layers_good)}_{str(dola_layers_bad)}.json', 'w') as json_file:
