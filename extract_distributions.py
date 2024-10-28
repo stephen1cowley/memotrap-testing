f6ef7b8de9dbf9ed70c11619ab04332290215475
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-10-28 16:22:15 +0000

    Basic probability distributions extraction

---
+++ extract_distributions.py
@@ -0,0 +1,30 @@
+from hybrid_method import HybridMethod
+import json
+from utils import renormalize_pmf
+
+NQ_DATAPATH = 'nq/orig_dev_filtered.json'
+MODEL = '4bit/Llama-2-7b-chat-hf'
+
+llm = HybridMethod(
+    model_name=MODEL,
+    device="cuda"
+)
+
+context = 'Write a quote that ends in the word ""affairs""'
+prompt = 'Mind your own'
+
+# context = 'Write a quote that ends in the word ""happy""'
+# prompt = 'The customer is always'
+
+all_distributions = llm.cad_dola_verbose_memotrap(
+    context=context,
+    prompt=prompt,
+    token_ids_of_interest=[1492, 9796, 297],
+    dola_layers_bad=None,
+    dola_layers_good=None,
+)
+
+with open('all_distributions.json', 'w') as json_file:
+    json.dump(all_distributions, json_file)
+    print("Successfully finished the experiment")
+    json_file.flush()
