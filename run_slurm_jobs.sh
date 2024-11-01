6f7d9a55206b0ff18500ee3a612e74f48af3099c
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-11-01 16:29:17 +0000

    run many slurm jobs with one command

---
+++ run_slurm_jobs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source ~/pytorch-env/bin/activate
+
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_none.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_low.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_high.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/low_none.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/low_low.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/low_high.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/high_none.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/high_low.wilkes3
+sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/high_high.wilkes3
