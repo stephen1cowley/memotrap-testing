506d97fe4a675b7651f96cbade6491b63fbb9d64
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-05-13 17:32:50 +0100

    ensuring all results will have time

--- long_test_nq.py
+++ long_test_nq.py
@@ -78,7 +78,8 @@ if __name__ == "__main__":
     ex_time = time.time() - time_0
     print(f"Model load time: {ex_time:.4f}s", flush=True)
 
-    betas: List[float] = [-1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+    # betas: List[float] = [-1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+    betas: List[float] = [2.0, 2.5, 3.0, 3.5, 4.0]
     # betas: List[float] = [-0.5]
     results: Dict[str, int] = {}
 
