f0de4f9eec09150bfc7e5f73fb22c3476b440f43
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-05-14 10:46:36 +0100

    redo incomplete

--- run_slurm_jobs.sh
+++ run_slurm_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /rds/user/ssc42/hpc-work/pytorch-env/bin/activate
 
-sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_none.wilkes3
-sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_low.wilkes3
+# sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_none.wilkes3
+# sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_low.wilkes3
 # sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/none_high.wilkes3
 sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/low_none.wilkes3
 sbatch /home/ssc42/ondemand/data/sys/myjobs/projects/default/$1/low_low.wilkes3
