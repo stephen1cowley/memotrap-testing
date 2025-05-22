1d3161f53014574fe1d9681760d599f1c6c57b30
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2025-05-22 10:04:11 +0100

    bug

--- run_tests_nq_prompt.sh
+++ run_tests_nq_prompt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export TRANSFORMERS_CACHE=/rds/user/ssc42/hpc-work
-python3 long_test_nq.py --dola-layers-good $1 --dola-layers-bad $2 --prompt-id $3
+python3 long_test_nq.py --dola-layers-good $1 --dola-layers-bad $2 --prompt_id $3
